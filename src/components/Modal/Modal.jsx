ec4db23078911b1ededc048d3069f6c1b715791d
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-11 07:52:48 +0530

    modal improved

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledModal = styled.div`
-  width: 300px;
+  width: 600px;
   height: 400px;
   padding: 15px;
   box-sizing: border-box;
-  background: #90caf9;
+  background: white;
   border-radius: 5px;
+  border: 1px solid #90caf9;
+  box-shadow: 1px 1px 1px #888;
+  z-index: 200;
+  position: fixed;
+  padding: 16px;
+  left: calc(50% - 300px);
+  top: 15%;
+  transition: all 0.3s ease-out;
+  transform: ${props => (props.show ? "translate(0)" : "translateY(-100vh)")};
+  opacity: ${props => (props.show ? "1" : "0")};
 `;
 
 const Modal = props => {
-  return <StyledModal />;
+  return <StyledModal show={props.show} />;
 };
 
 export default Modal;
