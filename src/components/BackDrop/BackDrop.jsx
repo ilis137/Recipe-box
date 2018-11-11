c5f0704d1983cef3a2480857290d73b8265467bf
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-11 08:06:12 +0530

    add backdrop

---
+++ src/components/BackDrop/BackDrop.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledBackDrop = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 120;
+  position: fixed;
+  background-color: #333;
+  top: 0;
+  left: 0;
+  transition: all 0.3s ease-out;
+  transform: ${props => (props.show ? "translate(0)" : "translateY(-100vh)")};
+  opacity: ${props => (props.show ? "0.5" : "0")};
+`;
+
+const BackDrop = props => {
+  return <StyledBackDrop show={props.show} onClick={props.canceled} />;
+};
+
+export default BackDrop;
