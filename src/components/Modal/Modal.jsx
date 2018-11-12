890ed8fc1577c89dacd6e3d7311f92f5c990ab84
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-12 07:23:47 +0530

    refactor for editpane

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,17 +3,19 @@ import styled from "styled-components";
 
 const StyledModal = styled.div`
   width: 600px;
-  height: 400px;
+  overflow: auto;
+  background-color: #90caf9;
+  height: 500px;
   padding: 15px;
   box-sizing: border-box;
-  background: white;
+
   border-radius: 5px;
   border: 1px solid #90caf9;
   box-shadow: 1px 1px 1px #888;
   z-index: 200;
   position: fixed;
   left: calc(50% - 300px);
-  top: 15%;
+  top: 5%;
   transition: all 0.3s ease-out;
   transform: ${props => (props.show ? "translate(0)" : "translateY(-100vh)")};
   opacity: ${props => (props.show ? "1" : "0")};
@@ -24,7 +26,7 @@ const StyledModal = styled.div`
 `;
 
 const Modal = props => {
-  return <StyledModal show={props.show} />;
+  return <StyledModal show={props.show}>{props.children}</StyledModal>;
 };
 
 export default Modal;
