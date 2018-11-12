54f9590f18d09de6a7192d84999ee54cfaa3f0ae
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-12 07:21:06 +0530

    refactor

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Search = props => {
-  console.log(props.recipes[1]);
+  //console.log(props.recipes[1]);
   const items = props.recipes.map((item, index) => (
     <option key={index} value={item.title.replace(/-/g, " ")}>
       {item.title.replace(/-/g, " ")}
@@ -10,8 +10,8 @@ const Search = props => {
     <form className="SelectRecipe" onChange={e => props.hitSearch(e)}>
       <label htmlFor="recipeName" />
       <select className={props.className} name="recipeName" id="recipeName">
-        <option key={0} value={" "}>
-          {" "}
+        <option key={0} value="Please Select A Recipe">
+          Please Select A Recipe
         </option>
         {items}
       </select>
